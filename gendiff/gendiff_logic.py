1708b2605b157f55036ebd6769e791fa3ee0e975
Anna Smertina <smr.anya@gmail.com>
2023-11-03 15:34:54 +0100

    Add generating diff for nested dictionaries, fix generate_diff

--- gendiff/gendiff_logic.py
+++ gendiff/gendiff_logic.py
@@ -15,46 +15,67 @@
 #         elif key in dict_2.keys():
 #             result.append(f'  + {key}: {value_2}')
 #     return '{\n' + '\n'.join(result) + '\n}'
+def get_all_keys(*dicts):
+    return sorted(list({y for x in dicts for y in list(x)}))
 
 
-def generate_diff(dict_1, dict_2):  # noqa: C901
+def _values_are_dicts(value_1, value_2):
+    return (value_1 is not None
+            and value_2 is not None
+            and isinstance(value_1, dict)
+            and isinstance(value_2, dict))
+
+
+def _value_changed(value_1, value_2):
+    return (value_1 is not None
+            and value_2 is not None
+            and value_1 != value_2
+            and (not isinstance(value_1, dict)
+                 or not isinstance(value_2, dict)))
+
+
+def _value_deleted(value_1, value_2):
+    return value_1 and value_2 is None
+
+
+def _value_added(value_1, value_2):
+    return value_1 is None and value_2
+
+
+def generate_description(value_1, value_2):
+    if value_1 == value_2:
+        return {
+            'status': 'unchanged',
+            'value': value_1
+        }
+    elif _values_are_dicts(value_1, value_2):
+        return {
+            'status': 'unchanged',
+            'children': generate_diff(value_1, value_2)
+        }
+    elif _value_changed(value_1, value_2):
+        return {
+            'status': 'changed',
+            'value1': value_1,
+            'value2': value_2
+        }
+    elif _value_deleted(value_1, value_2):
+        return {
+            'status': 'deleted',
+            'value': value_1
+        }
+    elif _value_added(value_1, value_2):
+        return {
+            'status': 'added',
+            'value': value_2
+        }
+
+
+def generate_diff(dict_1, dict_2):
     result = {}
-    keys_1 = list(dict_1)
-    keys_2 = list(dict_2)
-    all_keys = list({x for x in keys_1 + keys_2})
-    for key in sorted(all_keys):
+    all_keys = get_all_keys(dict_1, dict_2)
+    for key in all_keys:
         value_1 = dict_1.get(key)
         value_2 = dict_2.get(key)
-        if value_1 == value_2:
-            result[key] = {
-                'status': 'unchanged',
-                'value': value_1
-            }
-        elif (all([value_1, value_2])
-              and isinstance(value_1, dict)
-              and isinstance(value_2, dict)):
-            result[key] = {
-                'status': 'unchanged',
-                'children1': value_1,
-                'children2': value_2
-            }
-        elif (all([value_1, value_2])
-              and value_1 != value_2
-              and (not isinstance(value_1, dict)
-                   or not isinstance(value_2, dict))):
-            result[key] = {
-                'status': 'changed',
-                'value1': value_1,
-                'value2': value_2
-            }
-        elif value_1 and not value_2:
-            result[key] = {
-                'status': 'deleted',
-                'value': value_1
-            }
-        elif not value_1 and value_2:
-            result[key] = {
-                'status': 'added',
-                'value': value_2
-            }
+        result[key] = generate_description(value_1, value_2)
     return result
--- gendiff/tests/test_gendiff.py
+++ gendiff/tests/test_gendiff.py
@@ -1,12 +1,12 @@
 from gendiff import generate_diff
 from gendiff.tests.fixtures.parsed_files import (
-    FILE1_JSON, FILE2_JSON, EMPTY_JSON, FILE1_YAML, FILE2_YAML, EMPTY_YAML  # ,
-    # NESTED_FILE1_JSON, NESTED_FILE2_JSON,
-    # NESTED_FILE1_YAML, NESTED_FILE2_YAML
+    FILE1_JSON, FILE2_JSON, EMPTY_JSON, FILE1_YAML, FILE2_YAML, EMPTY_YAML,
+    NESTED_FILE1_JSON, NESTED_FILE2_JSON,
+    NESTED_FILE1_YAML, NESTED_FILE2_YAML
 )
 from gendiff.tests.fixtures.text_results import (
     DIFF_FLAT, DIFF_FLAT_SAME_FILE, DIFF_EMPTY_FIRST,
-    DIFF_EMPTY_SECOND, DIFF_BOTH_EMPTY  # , DIFF_NESTED
+    DIFF_EMPTY_SECOND, DIFF_BOTH_EMPTY, DIFF_NESTED
 )
 import pytest
 
@@ -37,11 +37,11 @@ def test_gendiff_flat_yaml(input_1, input_2, expected):
     assert generate_diff(input_1, input_2) == expected
 
 
-# @pytest.mark.parametrize(
-#     'input_1, input_2, expected',
-#     [(NESTED_FILE1_JSON, NESTED_FILE2_JSON, DIFF_NESTED),
-#      (NESTED_FILE1_YAML, NESTED_FILE2_YAML, DIFF_NESTED)
-#      ]
-# )
-# def test_gendiff_nested(input_1, input_2, expected):
-#     assert generate_diff(input_1, input_2) == expected
+@pytest.mark.parametrize(
+    'input_1, input_2, expected',
+    [(NESTED_FILE1_JSON, NESTED_FILE2_JSON, DIFF_NESTED),
+     (NESTED_FILE1_YAML, NESTED_FILE2_YAML, DIFF_NESTED)
+     ]
+)
+def test_gendiff_nested(input_1, input_2, expected):
+    assert generate_diff(input_1, input_2) == expected
